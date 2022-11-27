d42e2754f9d17a2b661884082006bf44597ed9f0
Jodoin, Nathan <nathanjodoin@my.unt.edu>
2022-11-27 14:17:17 -0600

    created tracker datastructure

---
+++ tracker.cpp
@@ -0,0 +1,37 @@
+/*
+CSCE 2110 Section 002/213
+Group 6 - Project 2
+Rock Paper Scissors
+Description: Win/Loss/Tie Datastructure Definition
+*/
+#include "tracker.hpp"
+#include <iostream>
+
+void tracker::P1_Win()
+{
+  p1.wins += 1;
+}
+
+void tracker::P2_Win()
+{
+  p2.wins += 1;
+}
+
+void tracker::Tie()
+{
+  ties += 1;
+}
+
+void tracker::print_results() const
+{
+  //Prints the results after calculating the various metrics.
+  unsigned long rounds = p1.wins + p2.wins + ties;
+  long double pct_wins = (long double)p1.wins / rounds * 100;
+  long double pct_ties = (long double)ties / rounds * 100;
+  long double pct_losses = (long double)p2.wins / rounds * 100;
+  
+  std::cout << "Rounds: " << rounds << std::endl;
+  std::cout << "Wins: " << p1.wins << " (" << pct_wins << "%)" << std::endl;
+  std::cout << "Ties: " << ties << " (" << pct_ties << "%)" << std::endl;
+  std::cout << "Losses: " << p2.wins << " (" << pct_losses << "%)" << std::endl;
+}
---
+++ tracker.hpp
@@ -0,0 +1,39 @@
+/*
+CSCE 2110 Section 002/213
+Group 6 - Project 2
+Rock Paper Scissors
+Description: Win/Loss/Tie Datastructure Declaration
+*/
+#ifndef _TRACKER_HPP_
+#define _TRACKER_HPP_
+
+typedef struct player
+{
+  unsigned int wins;
+}player;
+
+class tracker
+{
+  private:
+    player p1;
+    player p2;
+    unsigned int ties;
+  
+  public:
+    tracker()
+    {
+      //initialize values on the heap
+      p1.wins = 0;
+    
+      p2.wins = 0;
+    
+      ties = 0;
+    }
+   
+    void P1_Win();
+    void P2_Win();
+    void Tie();
+    void print_results() const;
+};
+
+#endif
