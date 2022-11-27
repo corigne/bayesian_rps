e72f79fc1aedb1b7b2da364f081332754770ff04
Jodoin, Nathan <nathanjodoin@my.unt.edu>
2022-11-27 14:17:40 -0600

    moved debug logic to a header

---
+++ debug.hpp
@@ -0,0 +1,33 @@
+/*
+CSCE 2110 Section 002/213
+Group 6 - Project 2
+Rock Paper Scissors
+Description: Main Function
+*/
+
+#ifndef _DEBUG_HPP_
+#define _DEBUG_HPP_
+
+#include <iostream>
+#include "tracker.hpp"
+
+void DEBUG(unsigned long& num_rounds, tracker& record)
+{
+  for(unsigned long i = 0; i < num_rounds; i++)
+        {
+          if(i % 2 == 0)
+          {
+            record.P1_Win();
+            std::cout << ".";
+            if(i == 80) std::cout << std::endl;
+          }
+          else
+          {
+            record.P2_Win();
+            std::cout << "-";
+          }
+        }
+        std::cout << std::endl;
+}
+
+#endif
